66bb0e268319b629e16682720eec8ecf3c988a11
Mertcan Kartal <69811127+mertcankartal@users.noreply.github.com>
2023-10-31 20:20:04 +0300

    Update Const.kt

--- app/src/main/java/com/example/moviecompose/util/const/Const.kt
+++ app/src/main/java/com/example/moviecompose/util/const/Const.kt
@@ -1,7 +1,7 @@
 package com.example.moviecompose.util.const
 
 object Const {
-    const val API_KEY = "ff9ae261"
+    
     //http://www.omdbapi.com/?apikey=[yourkey]&
     //parametreleri başlatırken ? ile başlatıyoruz. birden fazlaysa & işareti koyuyoruz.
     const val BASE_URL = "http://www.omdbapi.com"
